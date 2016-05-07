d178b9091c32835488d09aeea342af7fe3772fd9
Gemma Gotch <pveggie@hotmail.com>
2016-05-07 16:53:10 +0200

    Carrierwave integrated. some sample photos uploaded to cloudinary

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,21 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
 
   def show
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    @photo = Photo.new(photo_params)
+    @photo.image_alt = @photo.caption_title
+    if @photo.save
+      redirect_to photos_path
+    end
   end
 
   def edit
@@ -18,5 +25,15 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:image_location,
+                                  :image_location_cache,
+                                  :caption_title,
+                                  :caption_description,
+                                  :category,
+                                  :front_page)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
+  mount_uploader :image_location, ImageUploader
+
   scope :front_page, -> { where(front_page: true) }
 end
