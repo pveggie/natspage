1089ffe39610e10b456b9cf20fcaf22e6adcb9f7
Gemma Gotch <pveggie@hotmail.com>
2016-05-16 17:04:23 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+photo_images = [
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632705/zwimccmzgsh63vjsixzo.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632687/k2ybrz2zu6nuviakzatr.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632687/t8xgdfafkfzbztrfot5a.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632653/pqsca8t5nmhkaqighgup.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632631/jvussvvp2bsez7fkrosr.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632594/urs3txvccip5p9ut1scl.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632477/apzvlw27f6f6ubgr2uko.jpg",
+  "http://res.cloudinary.com/domva0jha/image/upload/v1462632289/spegog4xuuhzcrkommz8.jpg"
+]
+
+photo_images.each do |image|
+  Photo.create(remote_image_location_url: image)
+end
