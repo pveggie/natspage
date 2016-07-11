7823b47baa84511204177943765f54b114b5ff97
Gemma Gotch <pveggie@hotmail.com>
2016-07-11 20:24:08 +0200

    Add tests for Cloudinary storage and edit photo factory - Photo factory changed to have no image by default and have nested factories for local and remote photos. Tests for photo model updated accordingly. - Photo deletion added after each photo test to avoid having lots of test spam on Cloudinary. - Added methods for testing upload to and deletion from Cloudinary.

--- spec/factories/factory.rb
+++ spec/factories/factory.rb
@@ -9,17 +9,22 @@ FactoryGirl.define do
     end
   end
 
-
-
   factory :category do
     name Faker::Lorem.words(2)
   end
 
   factory :photo do
-    remote_image_location_url Faker::Avatar.image
     caption_title Faker::Lorem.words(1, false)
     caption_description Faker::Hipster.sentence(4)
     category_ids { create(:category)[:id] }
+
+    factory :remote_photo do
+      remote_image_location_url Faker::Avatar.image
+    end
+
+    factory :local_photo do
+      image_location File.open(File.join(Rails.root, '/spec/assets/test_image.jpg'))
+    end
   end
 
   factory :category_entry do
