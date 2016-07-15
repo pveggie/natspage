5f2291912b2596a9fbcd68c7e58d6289b054cef4
Gemma Gotch <pveggie@hotmail.com>
2016-07-15 15:03:39 +0200

    Spec for adding photo working. Need to add js driver for capybara though

--- spec/features/photos/create_photos_spec.rb
+++ spec/features/photos/create_photos_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "Create photos", :type => :feature do
-  let(:valid_attributes) { attributes_for(:local_photo) }
-  scenario "User is not logged in" do
-  end
+  feature "Adding Photos" do
+    background do
+      create(:admin)
+      create(:category, name: "Concerts")
+    end
+    after { Photo.destroy_all }
 
-  scenario "User is not admin" do
-  end
+    scenario "admin user can add a photo and then see it in the gallery" do
+      # go to index and click Add Photo
+      visit('/')
+      click_link("Add Photo")
 
-  scenario "User is logged in and admin" do
-    visit "/"
+      # add information to form and click to confirm
+      expect(page).to have_css("form")
+      within('form') do
+        fill_in "Caption title", with: "Rammstein"
+        fill_in "Caption description", with: "Performance at Jahrhunderthalle 2016"
+        attach_file("Image", File.join(Rails.root, "spec/assets/test_image.jpg"))
+        select("Concerts", from: "Categories")
+        click_button("Create Photo")
+      end
 
-    # click_button()
+      # be returned to index and see image
+      expect(page).to_not have_css("form")
+      expect(page).to have_content("Performance at Jahrhunderthalle 2016")
+      expect(page).to have_css(".photo-holder")
+    end
+
   end
 end
