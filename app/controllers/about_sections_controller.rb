17a58e21390d2ce24e98514de0a47dafe29dd158
Gemma Gotch <pveggie@hotmail.com>
2016-08-09 00:37:50 +0200

    Admin user can update about page text

--- app/controllers/about_sections_controller.rb
+++ app/controllers/about_sections_controller.rb
@@ -1,13 +1,29 @@
+# This is for updating the admin editable text in the about page (pages/about)
+# Displaying the about text is handled by the pages controller
 class AboutSectionsController < ApplicationController
+  before_action :confirm_admin
   before_action :find_about_section, only: [:update]
 
   def update
-    @about_section = AboutSection.find(params[:id])
+    @about_section.update(about_section_params)
+    if @about_section.save
+      redirect_to pages_about_path
+    else
+      render 'pages/about'
+    end
   end
 
   private
 
   def find_about_section
     @about_section = AboutSection.find(params[:id])
+  end
+
+  def about_section_params
+    params.require(:about_section).permit(:header, :content)
+  end
+
+  def confirm_admin
+    redirect_to pages_about_path unless admin_user?
   end
 end
--- spec/controllers/about_sections_controller_spec.rb
+++ spec/controllers/about_sections_controller_spec.rb
@@ -1,38 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe AboutSectionsController, focus: true do
+  let(:about_section) { create(:about_section) }
+
   let(:valid_attributes) { attributes_for(:about_section, header: "Updated")}
   let(:invalid_attributes) { attributes_for(:about_section, header: nil) }
 
   let(:admin) { create(:admin) }
 
   describe "PUT update" do
-    # context "when user is not a logged in admin" do
+    context "when user is not a logged in admin" do
 
-    #   it "does not update the text" do
-    #   end
-
-    #   it "stays on the about page" do
-    #   end
-    # end
+      it "does not update the text" do
+        header = about_section.header
+        put :update,
+          {
+            id: about_section.to_param,
+            about_section: valid_attributes
+          }
+        about_section.reload
+        expect(about_section.header).to eql(header)
+      end
+    end
 
     context "when user is admin" do
       before { sign_in(admin) }
 
-      it "assigns the new section as @about_section" do
-        admin_section = create(:admin_section)
-        put :update, { id: admin_section.to_param, admin_section: valid_attributes }
-        expect(assigns(:admin_section)).to eq(admin_section)
+      context "with valid_params" do
+        it "assigns the new section as @about_section" do
+          put :update, { id: about_section.to_param, about_section: valid_attributes }
+          expect(assigns(:about_section)).to eq(about_section)
+        end
+
+        it "updates the text" do
+          put :update, { id: about_section.to_param, about_section: valid_attributes }
+          about_section.reload
+          expect(about_section.header).to eql("Updated")
+        end
+
+        it "redirects to the about page" do
+          put :update, {id: about_section.to_param, about_section: valid_attributes }
+          expect(response).to redirect_to(pages_about_path)
+        end
       end
 
-      # it "updates the text" do
-      # end
+      context "with invalid params" do
+        it "does not update the about section" do
+          header = about_section.header
+          put :update,
+            {
+              id: about_section.to_param,
+              about_section: invalid_attributes
+            }
+          about_section.reload
+          expect(about_section.header).to eql(header)
+        end
 
-      # it "redirects to the about page" do
-      # end
+        it "renders the about page" do
+          put :update, { id: about_section.to_param, about_section: invalid_attributes }
+          expect(response).to render_template('pages/about')
+        end
+      end
     end
   end
 end
+
 
 
   # describe "PUT #update" do
@@ -62,11 +94,7 @@ end
   #         expect(assigns(:photo)).to eq(photo)
   #       end
 
-  #       it "redirects to the index" do
-  #         photo = Photo.create! valid_attributes
-  #         put :update, {:id => photo.to_param, :photo => valid_attributes}, valid_session
-  #         expect(response).to redirect_to(photos_path)
-  #       end
+  #
   #     end
 
   #     context "with invalid params" do
