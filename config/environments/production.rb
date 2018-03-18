3a43a2f026097169830faf9ea52ada6973830de0
Gemma Gotch <pveggie@hotmail.com>
2018-03-18 22:08:23 +0100

    Install figaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
-
   config.action_mailer.default_url_options = {
     host: "http://TODO_PUT_YOUR_DOMAIN_HERE"
   }
